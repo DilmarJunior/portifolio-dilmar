f5c7b28c845b2091c00558d8cc6013d3d3505879
Dilmar Mendes <dilmarjunior9@gmail.com>
2025-07-23 14:47:46 -0300

    feat: apagando tudo para recomeçar do 0

--- src/components/Icons/index.tsx
+++
@@ -1,9 +0,0 @@
-import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
-
-export default function Icon({ icon }: FontAwesomeIconProps) {
-  return (
-    <div>
-      <FontAwesomeIcon icon={icon} />
-    </div>
-  )
-}
--- src/pages/HomePage/index.tsx
+++
@@ -1,9 +0,0 @@
-import NavbarComponent from "@/components/Navbar";
-
-export default function HomePage() {
-  return (
-    <div>
-      <NavbarComponent />
-    </div>
-  );
-}
