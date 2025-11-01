c49b0f737b7da7a1be18e870ebddaccbcd723a5d
Dilmar Carvalho <dilmarjunior9@gmail.com>
2025-11-01 16:20:44 -0300

    feat: implementando i18n no projeto para fornecer tradução

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,60 @@
+import type { Metadata } from "next";
+import { NextIntlClientProvider, hasLocale } from "next-intl";
+import { notFound } from "next/navigation";
+import { routing } from "@/i18n/routing";
+import { setRequestLocale } from "next-intl/server";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import { getTranslations } from "next-intl/server";
+import "../globals.css";
+
+type Props = {
+  children: React.ReactNode;
+  params: { locale: string };
+};
+
+export async function generateStaticParams() {
+  const locales = routing.locales.map((locale) => ({ locale }));
+  return locales;
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { locale: string };
+}): Promise<Metadata> {
+  const { locale } = params;
+
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  const translations = await getTranslations({ locale, namespace: "metadata" });
+
+  return {
+    title: translations("meta-title"),
+    description: translations("meta-description"),
+  };
+}
+
+export default async function LocaleLayout({
+  children,
+  params,
+}: Readonly<Props>) {
+  const { locale } = await params;
+
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  setRequestLocale(locale);
+
+  return (
+    <html lang={locale}>
+      <body className="antialiased">
+        <NextIntlClientProvider>
+          <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
