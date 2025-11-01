589d65350d23838d11cb0285c5aa588821e8b3fa
Dilmar Carvalho <dilmar@bamse.co>
2025-11-01 03:37:56 -0300

    feat: iniciando projeto portifolio Dilmar

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "Portifolio - Dilmar Mendes Silva Carvalho",
+  description: "ortfólio de Dilmar Mendes Silva Carvalho, desenvolvedor de software full stack mobile. Inclui projetos, experiências e currículo profissional.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={'antialiased'}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
